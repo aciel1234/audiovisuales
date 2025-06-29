9e9f28d6e4b621ba462361098448e58310c92f8d
Aciel Olivera <acielito64@gmail.com>
2025-06-29 22:34:54 +0200

    asd

--- js/animes_filter.js
+++ js/animes_filter.js
@@ -41,3 +41,21 @@ totalPelis.innerText = `Total de animes: ${cantidad}`;
 totalPelis.style.background = 'rgba(0, 0, 0, 0.7)'
 totalPelis.style.margin = '-20px';
 totalPelis.style.textAlign = 'center';
+
+
+// Mostrar/ocultar el botón según el scroll
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btnSubir");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 200) {
+            btn.classList.add("visible");
+        } else {
+            btn.classList.remove("visible");
+        }
+    });
+
+    btn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
